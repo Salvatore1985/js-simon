1e5e88f0b291ed80a675f533ba0f812ec6fef4c9
Salvatore1985 <80257763+Salvatore1985@users.noreply.github.com>
2022-03-08 16:32:33 +0100

    funzione che genera numeri random dentro un array

--- js/script.js
+++ js/script.js
@@ -5,3 +5,26 @@ Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto pr
 Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
  */
 
+const numberCpu = [];
+let numberPage = generateRandomNumber(numberCpu, 5, 1, 100);
+console.log(numberPage);
+
+//  function che crea un numero random tra min e max e lo inserisci in un'array
+/**
+ *  function che crea un numero random tra min e max  e lo inserisci in un'array
+ * @param {*} numsArray è l'array dove vengono inseriti i numeri randomizati
+ * @param {number} min è il numero con il valore minimo
+ * @param {number} max è il numero con il valore massimo
+ */
+function generateRandomNumber(numsArray, number, min, max) {
+
+    for (let i = 0; i < number; i++) {
+        let randomInteger = Math.floor(Math.random() * ((max + 1) - min) + min);
+        numsArray.push(randomInteger);
+        console.log(numsArray);
+
+    }
+
+    return numsArray;
+
+}; 
